a9500202f3c6831d05779c681825985341b6f54e
FlutLab.io <>
2021-03-07 09:46:07 +0000

    Initial commit

---
+++ lib/game.dart
@@ -0,0 +1,138 @@
+import 'dart:math' as math;
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flame/gestures.dart';
+import 'package:flame/palette.dart';
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     GameWidget(
+//       game: MyGame(),
+//     ),
+//   );
+// }
+
+class Palette {
+  static const PaletteEntry white = BasicPalette.white;
+  static const PaletteEntry red = PaletteEntry(Color(0xFFFF0000));
+  static const PaletteEntry blue = PaletteEntry(Color(0xFF0000FF));
+}
+
+class Disc extends PositionComponent {
+  var speed = 1.0;
+  var speedX = 1.0;
+  bool flying = false;
+  var life = 1.0;
+  static const radius = 100.0;
+  static Offset startPos;
+
+  static Paint white = Palette.white.paint;
+  static Paint red = Palette.red.paint;
+  static Paint blue = Palette.blue.paint;
+
+  @override
+  void render(Canvas c) {
+    super.render(c);
+    white.style = PaintingStyle.stroke;
+    white.strokeWidth = math.pow(life, 4) * 20;
+    white.color = white.color.withOpacity(life);
+    c.drawOval(size.toRect(), white);
+    // c.drawRect(const Rect.fromLTWH(0, 0, 3, 3), red);
+    // c.drawRect(Rect.fromLTWH(width / 2, height / 2, 3, 3), blue);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position.y += speed;
+    if (flying) {
+      speed *= 0.97;
+      life -= 0.001;
+      if (life <= 0) {
+        remove();
+      }
+    }
+  }
+
+  @override
+  void onMount() {
+    anchor = Anchor.center;
+    super.onMount();
+    size = Vector2.all(radius);
+    position = Vector2(startPos.dx, startPos.dy);
+  }
+
+  void changeSpeed(double s) {
+    if (s.abs() > speed.abs()) speed = s / 100;
+    print(s);
+  }
+}
+
+class MyGame extends BaseGame with DoubleTapDetector, TapDetector, VerticalDragDetector {
+  bool running = true;
+  Disc currentDisc;
+  FPSCounter fpsCounter;
+  MyGame() {
+    Disc.startPos = window.physicalSize.bottomCenter(Offset.zero);
+
+    add(Disc()
+      ..x = 200
+      ..y = 100);
+  }
+  Component isTouched(Offset pos) {
+    final touchArea = Rect.fromCenter(
+      center: pos,
+      width: 20,
+      height: 20,
+    );
+
+    // final handled = components.firstWhere((c) {
+    for (var c in components) {
+      if (c is PositionComponent && c.toRect().overlaps(touchArea)) {
+        return c;
+      }
+      // remove(c);
+      // return true;
+    }
+    // });
+    return null;
+  }
+
+  @override
+  void onVerticalDragDown(DragDownDetails details) {}
+  @override
+  void onVerticalDragStart(DragStartDetails details) {}
+
+  @override
+  void onVerticalDragEnd(DragEndDetails details) {
+    currentDisc?.flying = true;
+    // currentDisc.speed = details.velocity.pixelsPerSecond.dy;
+    currentDisc?.changeSpeed(details.velocity.pixelsPerSecond.dy);
+
+    add(Disc());
+  }
+
+  @override
+  void onVerticalDragUpdate(DragUpdateDetails details) {
+    var disc = isTouched(details.localPosition);
+    if (disc == null) return;
+    currentDisc = (disc as Disc);
+    currentDisc.position = Vector2(details.localPosition.dx, details.localPosition.dy);
+    // (disc as Disc)?.changeSpeed(details.delta.dy);
+    // print(details.localPosition);
+  }
+
+  @override
+  void onDoubleTap() {
+    if (running) {
+      pauseEngine();
+    } else {
+      resumeEngine();
+    }
+
+    running = !running;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
+import 'package:rive/rive.dart';
+
+import 'game.dart';
+import 'wiper_controller.dart';
+
+// void main() => runApp(MyApp());
+void set120Hz() async {
+  try {
+    var modes = await FlutterDisplayMode.supported;
+    modes.forEach(print);
+
+    /// On OnePlus 7 Pro:
+    /// #1 1080x2340 @ 60Hz
+    /// #2 1080x2340 @ 90Hz
+    /// #3 1440x3120 @ 90Hz
+    /// #4 1440x3120 @ 60Hz
+
+    /// On OnePlus 8 Pro:
+    /// #1 1080x2376 @ 60Hz
+    /// #2 1440x3168 @ 120Hz
+    /// #3 1440x3168 @ 60Hz
+    /// #4 1080x2376 @ 120Hz
+    await FlutterDisplayMode.setMode(modes.last);
+  } on PlatformException catch (e) {
+    /// e.code =>
+    /// noAPI - No API support. Only Marshmallow and above.
+    /// noActivity - Activity is not available. Probably app is in background
+  }
+}
+
+void main() async {
+  final game = ();MyGameMyGame/MyGame/MyGamMyGamMyGamMyGaMyGamMyGaMyGamMyGamMyGamMyGaMyGamMyGMyGamMyGamMyGamMyGaMyGamMyG
+MyGame
+MyGame
+
+  // await game.onLoad();
+  runApp(
+    GameWidget(
+      // game: MyGame(),
+      game: game,
+    ),
+  );
+  set120Hz();
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gone fishing.',
+      home: Scaffold(
+        body: MyRiveAnimation(),
+      ),
+    );
+  }
+}
+
+class MyRiveAnimation extends StatefulWidget {
+  @override
+  _MyRiveAnimationState createState() => _MyRiveAnimationState();
+}
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _MyRiveAnimationState extends State<MyRiveAnimation> {
+  final riveFileName = 'assets/pong.riv';
+  Artboard _artboard;
+  WiperAnimation _wipersController;
+  // Flag to turn wipers on and off
+  bool _wipers = false;
+
+  @override
+  void initState() {
+    _loadRiveFile();
+    super.initState();
+  }
+
+  void _loadRiveFile() async {
+    final bytes = await rootBundle.load(riveFileName);
+    final file = RiveFile();
+
+    if (file.import(bytes)) {
+      setState(() => _artboard = file.mainArtboard
+        ..addController(
+          SimpleAnimation('Animation 1'),
+        ));
+    }
+  }
+
+  void _wipersChange(bool wipersOn) {
+    if (_wipersController == null) {
+      _artboard.addController(
+        _wipersController = WiperAnimation('windshield_wipers'),
+      );
+    }
+    if (wipersOn) {
+      _wipersController.start();
+    } else {
+      _wipersController.stop();
+    }
+    setState(() => _wipers = wipersOn);
+  }
+
+  // final bool active;
+  // final ValueChanged<bool> onChanged;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: GestureDetector(
+            onVerticalDragUpdate: (details) {
+              print("tap");
+              Shape shape = _artboard.children.firstWhere(
+                  (element) => element.coreType == Shape().coreType);
+              shape.scaleY += (details.delta.dy / 100);
+            },
+            onTap: () {
+              doit();
+            },
+            child: _artboard != null
+                ? Rive(
+                    artboard: _artboard,
+                    fit: BoxFit.cover,
+                  )
+                : Container(),
+          ),
+        ),
+        SizedBox(
+          height: 50,
+          width: 200,
+          child: SwitchListTile(
+            title: const Text('Wipers'),
+            value: _wipers,
+            onChanged: _wipersChange,
+          ),
+        ),
+      ],
+    );
+  }
+
+  doit() {
+    print("hej");
+    // _artboard.path.transform(Transform.scale(scale: 0.9).);
+  }
+}
